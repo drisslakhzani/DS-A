1e93b383e86151d587a77749c0eefb65fb04eb80
drisslakhzani <driss.lakhzani.lkz@gmail.com>
2023-12-16 13:17:47 +0100

    ds:stack

---
+++ PYTHON/Stack.py
@@ -0,0 +1,12 @@
+# the collection (deque way of using the stack and its similar to the table thing )
+from collections import deque   
+
+stack=deque()
+
+stack.append("Minecraft")
+stack.append("Skyrim")
+stack.append("DOOM")
+stack.append("Borderlands")
+stack.append("FFVII")
+FavGame= stack.index("FFVII")
+# print(FavGame)   
